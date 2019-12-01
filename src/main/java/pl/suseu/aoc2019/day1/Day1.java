2333bf684db0b28ef2488425287ea9d6973b63f8
BuildTools <szymonwitek02@gmail.com>
2019-12-01 12:53:36 +0100

    Add solution to second part of day 1

--- src/main/java/pl/suseu/aoc2019/day1/Day1.java
+++ src/main/java/pl/suseu/aoc2019/day1/Day1.java
@@ -3,6 +3,7 @@ package pl.suseu.aoc2019.day1;
 import pl.suseu.aoc2019.FileUtils;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day1 {
 
@@ -10,8 +11,27 @@ public class Day1 {
 
     public void firstPart(){
         masses = FileUtils.loadFileAsIntegerList("day1");
-        int sum = masses.stream().mapToInt(i -> i / 3 - 2).sum();
+        int sum = masses.stream().mapToInt(this::calculateFuel).sum();
         System.out.println(sum);
+    }
+
+    public void secondPart(){
+        masses = FileUtils.loadFileAsIntegerList("day1");
+        AtomicInteger fuelNeeded = new AtomicInteger();
+        masses.forEach(mass -> {
+            int fuel = calculateFuel(mass);
+            int fuel2 = calculateFuel(fuel);
+            while (fuel2 > 0) {
+                fuel += fuel2;
+                fuel2 = calculateFuel(fuel2);
+            }
+            fuelNeeded.addAndGet(fuel);
+        });
+        System.out.println(fuelNeeded);
+    }
+
+    private int calculateFuel(int mass) {
+        return mass / 3 - 2;
     }
 
 }
